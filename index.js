357356ac0b402a177d83cfe0f063e2550776dd6f
MaruhanSelva <75972624+MaruhanSelva@users.noreply.github.com>
2022-09-12 22:43:39 -0400

    added more styling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let mode = "draw";
 let mouseDown = false;
 let block = "images/oak_plank.png";
 let curBlock = document.querySelector(".curBlock");
+let blocks = new Map();
 
 
 const container = document.querySelector('#container');
@@ -17,8 +18,8 @@ function drawGrid(number) {
     for (let i = 0; i < number * number; i++) {
         const content = document.createElement('div');
         content.classList.add('content');
-        content.style.width = 400 / number + 'px';
-        content.style.height = 400 / number + 'px';
+        content.style.width = 500 / number + 'px';
+        content.style.height = 500 / number + 'px';
         container.appendChild(content);
         content.addEventListener('mouseenter', gridColour);
         content.addEventListener('mousedown', (e) => {
@@ -38,11 +39,25 @@ function mouseState() {
 
 function gridColour(e) {
     if (mouseDown == true) {
+        const old_style = String(getComputedStyle(e.target).backgroundImage);
         if (mode === "draw") {
+            if (old_style != "none") {
+                blocks.set(old_style, blocks.get(old_style) - 1);
+            }
             e.target.style.backgroundImage = "url(" + block + ")";
-            e.target.style.backgroundSize = "" + (400/number) + "px " + (400/number) + "px";
+            e.target.style.backgroundSize = "" + (500/number) + "px " + (500/number) + "px";
+
+            const new_style = String(getComputedStyle(e.target).backgroundImage);
+            if (blocks.has(new_style) === false) {
+                blocks.set(new_style, 1);
+            } else {
+                blocks.set(new_style, blocks.get(new_style) + 1);
+            }
             //e.target.style.background = 'black';
         } else {
+            if (old_style != "none") {
+                blocks.set(old_style, blocks.get(old_style) - 1);
+            }
             e.target.style.backgroundImage = 'none';
            // e.target.style.background = 'white';
         }
@@ -50,6 +65,7 @@ function gridColour(e) {
 }
 
 function clearGrid() {
+    blocks.clear();
     document.querySelectorAll(".content").forEach(el => el.remove());
 }
 
@@ -69,6 +85,7 @@ sizeButton.addEventListener('click', (e) => {
 
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener('click', (e) => {
+    blocks.clear();
     const cells = document.querySelectorAll(".content").forEach(el => 
         el.style.background = 'white')
 });
@@ -90,104 +107,136 @@ function blockUpdate() {
     }
 }
 
+const blockWindow = document.querySelector("#blockCount");
+
+function blockCount() {
+    for (const [key, value] of blocks) {
+        const item = document.createElement('div');
+        
+    }
+
+}
+
+const calculateButton = document.querySelector("#calculate");
+calculateButton.addEventListener('click', () => {
+    blockCount();
+});
+
+
+
 
 // Logic For Buttons
 const oak_wood = document.querySelector("#oak_w");
 oak_wood.addEventListener('click', () => {
     block = "images/oak.jpeg";
+    mode = "draw";
     blockUpdate();
 });
 
 const oak_plank = document.querySelector("#oak_p");
 oak_plank.addEventListener('click', () => {
     block = "images/oak_plank.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const birch_plank = document.querySelector("#birch_p");
 birch_plank.addEventListener('click', () => {
     block = "images/birch_plank.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const birch_wood = document.querySelector("#birch_w");
 birch_wood.addEventListener('click', () => {
     block = "images/birch.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const acacia_wood = document.querySelector("#acacia_w");
 acacia_wood.addEventListener('click', () => {
     block = "images/acacia.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const acacia_plank = document.querySelector("#acacia_p");
 acacia_plank.addEventListener('click', () => {
     block = "images/acacia_plank.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const stone_b = document.querySelector("#stone_b");
 stone_b.addEventListener('click', () => {
     block = "images/stone_brick.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const cobble = document.querySelector("#cobble");
 cobble.addEventListener('click', () => {
     block = "images/cobble.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const sand = document.querySelector("#sand");
 sand.addEventListener('click', () => {
     block = "images/sand.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const red_w = document.querySelector("#red_w");
 red_w.addEventListener('click', () => {
     block = "images/red_w.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const black_w = document.querySelector("#black_w");
 black_w.addEventListener('click', () => {
     block = "images/black_w.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const magenta_w = document.querySelector("#magenta_w");
 magenta_w.addEventListener('click', () => {
     block = "images/magenta_w.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const lime_w = document.querySelector("#lime_w");
 lime_w.addEventListener('click', () => {
     block = "images/lime_w.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const dirt = document.querySelector("#dirt");
 dirt.addEventListener('click', () => {
     block = "images/dirt.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const diamond = document.querySelector("#diamond");
 diamond.addEventListener('click', () => {
     block = "images/diamond.png";
+    mode = "draw";
     blockUpdate();
 });
 
 const gold = document.querySelector("#gold");
 gold.addEventListener('click', () => {
     block = "images/gold.png";
+    mode = "draw";
     blockUpdate();
 });
-
 
 
 drawGrid(16);
